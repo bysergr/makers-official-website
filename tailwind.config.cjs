a99011453d89cc52e08b1593e6a302717bd2973f
Sergio Rey <reysergio383@gmail.com>
2024-07-11 00:15:37 -0500

    refactor: Update Tailwind configuration and remove unused code

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-const withMT = require("@material-tailwind/react/utils/withMT");
 
-module.exports = withMT({
+module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   presets: [],
   darkMode: 'media', // or 'class'
@@ -969,4 +968,4 @@ module.exports = withMT({
     'disabled',
   ],
   plugins: [],
-});
+};
