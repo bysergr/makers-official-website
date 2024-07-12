fdb7fc810419b97090c473225ee16bb21aa1a84e
Sergio Rey <reysergio383@gmail.com>
2024-07-12 00:51:28 -0500

    Add fellows section, add toggle theme button and fix responsive design

--- src/components/ui/QuestionItem.jsx
+++ src/components/ui/QuestionItem.jsx
@@ -22,12 +22,12 @@ function QuestionItem({ title, answer }) {
     <Disclosure>
       {({ open }) => (
         <>
-          <Disclosure.Button className="my-6 font-semibold text-lg text-left flex gap-2 justify-between items-center w-full py-3 px-4 bg-white hover:bg-gray-100 transition-colors duration-300 rounded-lg shadow-md">
+          <Disclosure.Button className="my-6 dark:text-white font-semibold text-lg text-left flex gap-2 justify-between items-center w-full px-4 transition-colors duration-300 rounded-lg">
             <p className="block max-w-[90%]">{title}</p>
             <ChevronDownIcon className={clsx("w-6 h-6 transition-transform", open ? "rotate-180" : "rotate-0")} />
           </Disclosure.Button>
           <Animation>
-            <Disclosure.Panel className="text-gray-800 my-4 px-4">
+            <Disclosure.Panel className="text-gray-800 dark:text-gray-200 my-6 px-4">
               {answer}
             </Disclosure.Panel>
           </Animation>
